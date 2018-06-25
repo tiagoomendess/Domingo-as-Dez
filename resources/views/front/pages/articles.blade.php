02085c76eb0fca42d91e08c13dfc69d9163baa86
tiagoomendess <tiago@mendes.com.pt>
2018-06-25 18:31:29 +0100

    Alterada a pagina de noticias

--- resources/views/front/pages/articles.blade.php
+++ resources/views/front/pages/articles.blade.php
@@ -2,32 +2,31 @@
 
 @section('head-content')
     <title>{{ trans('general.news') }}</title>
+    <link rel="stylesheet" href="/css/front/articles-style.css">
 @endsection
 
 @section('content')
 
     <div class="container">
-        <div class="row">
+        <div class="row no-margin-bottom">
             @foreach($articles as $article)
                 <div class="col s12 m6 l4">
 
                     <div class="card medium hoverable">
                         <div class="card-image">
-                            @if($article->media)
-                                @if($article->media->media_type == 'image')
-                                    <img src="{{ $article->media->url }}" alt="{{ $article->media->tags }}">
-                                @elseif($article->media->media_type == 'youtube')
-                                    <div class="embed-container">
-                                        <iframe id="media_youtube_video" style="width: 100%" src="{{ str_replace('watch?v=', 'embed/', $article->media->url . '&rel=0&amp;controls=0&amp;showinfo=0&amp;') }}" frameborder='0' allowfullscreen></iframe>
-                                    </div>
+                            <div class="article-thumb-fill">
+                                @if($article->media)
+                                    @if($article->media->media_type == 'image')
+                                        <img src="{{ $article->media->url }}" alt="{{ $article->media->tags }}">
+                                    @else
+                                        <img src="{{ \App\Media::getPlaceholder('16:9', $article->id) }}" alt="">
+                                    @endif
+
                                 @else
                                     <img src="{{ \App\Media::getPlaceholder('16:9', $article->id) }}" alt="">
                                 @endif
-
-                            @else
-                                <img src="{{ \App\Media::getPlaceholder('16:9', $article->id) }}" alt="">
-                            @endif
-                            <span class="card-title" style="text-shadow: 1px 1px 3px #000000;">{{ $article->title }}</span>
+                                <span class="card-title">{{ $article->title }}</span>
+                            </div>
                         </div>
                         <div class="card-content">
                             <p class="">{{ str_limit($article->description, 300) }}</p>
