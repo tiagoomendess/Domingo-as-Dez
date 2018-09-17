6bccc51f0d3b71686b929b3add5e2700e5e748f7
tiagoomendess <tiago@mendes.com.pt>
2018-09-17 17:46:54 +0100

    FAVICON

--- resources/views/front/layouts/default-page.blade.php
+++ resources/views/front/layouts/default-page.blade.php
@@ -9,6 +9,15 @@
     <link type="text/css" rel="stylesheet" href="/css/front/navbar-style.css"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="/css/front/footer-style.css"  media="screen,projection"/>
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&amp;subset=latin-ext" rel="stylesheet">
+    <!-- FAVICON-->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="theme-color" content="#ffffff">
+
     @yield('head-content')
 @endsection
 
