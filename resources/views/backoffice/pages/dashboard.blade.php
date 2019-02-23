eeee33c803390ac4b98b91dde53d02a81a8f925b
tiagoomendess <tiago@mendes.com.pt>
2019-02-23 22:51:47 +0000

    dashboard changed 6

--- resources/views/backoffice/pages/dashboard.blade.php
+++ resources/views/backoffice/pages/dashboard.blade.php
@@ -11,8 +11,8 @@
         <div class="col s12">
             <h4>Tutoriais:</h4>
             <p class="flow-text">Isto é uma playlist de vídeos a explicar como realizar algumas tarefas no website.
-                Cosoante as permissões associadas à sua conta,
-                poderá não conseguir virualizar todas as opções que aparecem nos vídeos. Se preferir pode visualizar os
+                Consoante as permissões associadas à sua conta,
+                poderá não conseguir visualizar todas as opções que aparecem nos vídeos. Se preferir pode visualizar os
                 vídeos diretamente no youtube clicando no link:
                 <a href="https://www.youtube.com/watch?v=iuVF45lBRxY&list=PL_GkJl6tG6PckYIYCDM5RtLCc38L63lQk">https://www.youtube.com/watch?v=iuVF45lBRxY&list=PL_GkJl6tG6PckYIYCDM5RtLCc38L63lQk</a>
             </p>
