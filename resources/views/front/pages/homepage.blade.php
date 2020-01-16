e67d75d49432c83ea840db060cd674f8f80c7633
tiagoomendess <tiago@mendes.com.pt>
2020-01-16 22:21:44 +0000

    asdsa

--- resources/views/front/pages/homepage.blade.php
+++ resources/views/front/pages/homepage.blade.php
@@ -233,7 +233,6 @@
         $(document).ready(function () {
             // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
             $('.modal').modal();
-            $('.carousel.carousel-slider').carousel({fullWidth: true});
         });
     </script>
 @endsection
