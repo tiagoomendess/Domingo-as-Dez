c7602b35af8b51c42b8177baf7b4f494190a08e0
Tiago Mendes <tiagoomendess@gmail.com>
2023-07-29 00:25:51 +0100

    Added google analytics

---
+++ resources/views/front/partial/google_analytics.blade.php
@@ -0,0 +1,14 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PPBRM1T8VZ"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'G-PPBRM1T8VZ');
+</script>
+<!-- END Google tag (gtag.js) -->
