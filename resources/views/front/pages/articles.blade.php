43eed61c5fa19cfa4598914bb1793df7e697fbc0
Tiago Mendes <tiagoomendess@gmail.com>
2023-12-31 01:44:39 +0000

    test 5

--- resources/views/front/pages/articles.blade.php
+++ resources/views/front/pages/articles.blade.php
@@ -4,9 +4,9 @@
     <title>{{ trans('general.news') }}</title>
     <link rel="stylesheet" href="/css/front/articles-style.css">
 
-    <meta property="og:title" content="{{ trans('general.news') . ' ' . config('app.name') }}" />
-    <meta property="og:type" content="website" />
-    <meta property="og:description" content="{{ trans('front.footer_desc') }}" />
+    <meta property="og:title" content="{{ trans('general.news') . ' ' . config('app.name') }}"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:description" content="{{ trans('front.footer_desc') }}"/>
 
 @endsection
 
@@ -24,47 +24,47 @@
                 @if($index === 4 && !has_permission('disable_ads'))
                     <div class="col xs12 s12 m6 l6 xl4">
                         <div class="square-ad" style="margin-top: 10px">
-                            <div>
-                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3518000096682897"
-                                        crossorigin="anonymous"></script>
-                                <!-- News Page Card -->
-                                <ins class="adsbygoogle"
-                                     style="display:block"
-                                     data-ad-client="ca-pub-3518000096682897"
-                                     data-ad-slot="3616731365"
-                                     data-ad-format="auto"
-                                     data-full-width-responsive="true"></ins>
-                                <script>
-                                    (adsbygoogle = window.adsbygoogle || []).push({});
-                                </script>
-                            </div>
+                            <script async
+                                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3518000096682897"
+                                    crossorigin="anonymous"></script>
+                            <!-- News Page Card -->
+                            <ins class="adsbygoogle"
+                                 style="display:block"
+                                 data-ad-client="ca-pub-3518000096682897"
+                                 data-ad-slot="3616731365"
+                                 data-ad-format="auto"
+                                 data-full-width-responsive="true"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
                         </div>
                     </div>
                 @endif
                 <div class="col xs12 s12 m6 l6 xl4">
-                        <div class="card medium hoverable">
-                            <div class="card-image">
-                                <div class="article-thumb-fill">
-                                    @if($article->media)
-                                        <img src="{{ $article->media->thumbnail_url ? $article->media->thumbnail_url : \App\Media::getPlaceholder('16:9', $article->id) }}" alt="{{ $article->media->tags }}">
-                                    @else
-                                        <img src="{{ \App\Media::getPlaceholder('16:9', $article->id) }}" alt="">
-                                    @endif
-                                    <span class="card-title">{{ $article->title }}</span>
-                                </div>
+                    <div class="card medium hoverable">
+                        <div class="card-image">
+                            <div class="article-thumb-fill">
+                                @if($article->media)
+                                    <img src="{{ $article->media->thumbnail_url ? $article->media->thumbnail_url : \App\Media::getPlaceholder('16:9', $article->id) }}"
+                                         alt="{{ $article->media->tags }}">
+                                @else
+                                    <img src="{{ \App\Media::getPlaceholder('16:9', $article->id) }}" alt="">
+                                @endif
+                                <span class="card-title">{{ $article->title }}</span>
                             </div>
-                            <div class="card-content">
-                                <p class="">{{ \Illuminate\Support\Str::limit($article->description, 300) }}</p>
-                            </div>
-                            <div class="card-action">
-                                <a href="{{ route('news.show', [
+                        </div>
+                        <div class="card-content">
+                            <p class="">{{ \Illuminate\Support\Str::limit($article->description, 300) }}</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="{{ route('news.show', [
                             'year' => \Carbon\Carbon::createFromFormat("Y-m-d H:i:s", $article->date)->year,
                             'month' => \Carbon\Carbon::createFromFormat("Y-m-d H:i:s", $article->date)->month,
                             'day' => \Carbon\Carbon::createFromFormat("Y-m-d H:i:s", $article->date)->day,
                             'slug' => str_slug($article->title)
                         ]) }}" class="right blue-text">{{ trans('front.read_more') }}</a>
-                            </div>
                         </div>
+                    </div>
 
                 </div>
 
