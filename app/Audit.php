15df8cf8e76b12bc6b50400462b347277a925cca
Tiago Mendes <tiagoomendess@gmail.com>
2024-01-04 23:40:54 +0000

    testing some stufss

--- app/Audit.php
+++ app/Audit.php
@@ -225,8 +225,10 @@ class Audit extends SearchableModel {
         }
 
         $ip = request()->getClientIp();
+        Log::info("Got IP From Request: $ip");
         if (empty($ip)) {
             $ip = !empty($_COOKIE['ip']) ? Str::limit($_COOKIE['ip'], 45, '') : '';
+            Log::info("Got IP From Cookie: $ip");
         }
 
         $lang = null;
