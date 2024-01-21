7a27699356765faa3ac0cececbf443435eb2133f
Tiago Mendes <tiagoomendess@gmail.com>
2024-01-21 01:11:24 +0000

    email

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@domingoasdez.com'),
+        'name' => env('MAIL_FROM_NAME', 'Domingo às Dez'),
     ],
 
     /*
