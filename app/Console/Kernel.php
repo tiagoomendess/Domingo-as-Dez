85ba661a7826cb47e0de515039361c277161d0a1
Tiago Mendes <tiagoomendess@gmail.com>
2024-01-27 16:33:01 +0000

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
         $schedule->job(new GenerateGameImage())->everyMinute();
         $schedule->job(new ProcessPolls())->everyMinute();
         $schedule->job(new ProcessScoreReportBans())->dailyAt('22:59');
-        $schedule->job(new ScoreReportConsumer())->everyFiveMinutes();
+        $schedule->job(new ScoreReportConsumer())->everyMinute();
     }
 
     /**
