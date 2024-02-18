95a069443f8d2b85dd2d78c3d4944c4a1cecbbd1
Tiago Mendes <tiagoomendess@gmail.com>
2024-02-18 21:24:46 +0000

    fixed bug 2

--- app/Http/Controllers/Front/MvpVotesController.php
+++ app/Http/Controllers/Front/MvpVotesController.php
@@ -9,9 +9,11 @@ use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\MessageBag;
+use Illuminate\Support\Str;
 
 class MvpVotesController extends Controller
 {
@@ -51,6 +53,14 @@ class MvpVotesController extends Controller
                 'player_id' => $player_id,
                 'user_id' => $user->id
             ]);
+
+            $competition_slug = Str::slug($game->game_group->season->competition->name);
+            $season_slug = $game->game_group->season->getNameSlug();
+            $group_slug = Str::slug($game->game_group->name);
+            $round = $game->round;
+            $clubs_slug = Str::slug($game->home_team->club->name . '-vs-' . $game->away_team->club->name);
+            $cache_key = "game-cache-$competition_slug-$season_slug-$group_slug-$round-$clubs_slug";
+            Cache::store('file')->forget($cache_key);
 
             Log::info("User " . $user->id . " voted for player " . $player_id . " for MVP in the game " . $game_id);
 
