45c4100c2c3fee0869b528888c3aa756598da99c
Tiago Mendes <tiagoomendess@gmail.com>
2025-02-27 12:41:43 +0000

    added consent props

--- resources/views/front/partial/google_analytics.blade.php
+++ resources/views/front/partial/google_analytics.blade.php
@@ -2,13 +2,17 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-PPBRM1T8VZ"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
 
-    function gtag() {
-        dataLayer.push(arguments);
-    }
+    // Initialize default consent state for both ad and analytics storage
+    gtag('consent', 'default', {
+        'ad_storage': 'granted',
+        'analytics_storage': 'granted'
+    });
 
     gtag('js', new Date());
 
+    // Configure your Google Analytics tag
     gtag('config', 'G-PPBRM1T8VZ');
 </script>
 <!-- END Google tag (gtag.js) -->
